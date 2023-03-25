720a91a6aa391ad3eebf2ee76f355d04b000a2e1
ArkaBarua173 <40537823+Arka-Barua@users.noreply.github.com>
2023-03-25 21:42:46 +0600

    Profile forum page created

---
+++ pages/profile/forum.js
@@ -0,0 +1,11 @@
+import UserProfileLayout from "@/components/UserProfileLayout";
+
+export default function Profile() {
+  return (
+    <>
+      <UserProfileLayout>
+        <h1 className="items-start">Forum</h1>
+      </UserProfileLayout>
+    </>
+  );
+}
