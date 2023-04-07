dcaccad7317b0c77663004580c122a13c4d017df
ArkaBarua173 <40537823+Arka-Barua@users.noreply.github.com>
2023-04-07 17:27:22 +0600

    Profile forum page created

--- pages/profile/forum.js
+++ pages/profile/forum.js
@@ -1,10 +1,40 @@
+import Loading from "@/components/Loading";
+import SingleForum from "@/components/SingleForum";
 import UserProfileLayout from "@/components/UserProfileLayout";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-export default function Profile() {
+const getUserTopics = async () => {
+  const response = await axios.get("/api/forum/userForum");
+  return response.data;
+};
+
+export default function UserForum() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: getUserTopics,
+    queryKey: ["getUserTopics"],
+  });
+
+  if (error) return error;
+  if (isLoading) return <Loading />;
+
+  console.log(data);
+
   return (
     <>
       <UserProfileLayout>
-        <h1 className="items-start">Forum</h1>
+        <div className="my-8 ml-8">
+          <h1 className="text-2xl font-bold my-8">Forum</h1>
+          <p className="font-semibold">
+            You started {data?.topic?.length} discussions and got{" "}
+            {data?.commentCount} comments
+          </p>
+        </div>
+        {data?.topic?.map((datum) => (
+          <div className="m-8 card bg-base-200" key={datum.id}>
+            <SingleForum topic={datum} />
+          </div>
+        ))}
       </UserProfileLayout>
     </>
   );
