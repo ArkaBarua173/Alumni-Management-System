5660292bffdee189dcc9db426db3c46f137606be
ArkaBarua173 <40537823+Arka-Barua@users.noreply.github.com>
2023-04-07 17:25:07 +0600

    Page skeleton for admin dashboard pages

---
+++ pages/admin/alumni.js
@@ -0,0 +1,4 @@
+import AdminDashboard from "@/components/AdminDashboard";
+export default function Alumni() {
+  return <AdminDashboard></AdminDashboard>;
+}
---
+++ pages/admin/users.js
@@ -0,0 +1,4 @@
+import AdminDashboard from "@/components/AdminDashboard";
+export default function Users() {
+  return <AdminDashboard></AdminDashboard>;
+}
